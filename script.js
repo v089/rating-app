2e7850caba0c025f7a90225233034dd96fd48d3e
Unknown <milovjope@cistit.xyu>
2024-10-15 10:55:09 +0900

    end second lesson

--- script.js
+++ script.js
@@ -20,7 +20,33 @@ const swiper = new Swiper('.swiper', {
     },
   
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
   });
+
+
+
+
+
+const second = document.querySelector(".second-slider");
+const third = document.querySelector(".third-slider");
+const buttonSecond = document.getElementById("loadMore");
+const buttonThird = document.getElementById("loadMore1");
+
+buttonSecond.addEventListener("click", () => {
+    if (second.style.display === "none" || second.style.display === "") {
+        second.style.display = "block";
+        buttonSecond.innerText = "Скрыть"
+    } else {
+        second.style.display = "none"; // Скрываем элемент
+        buttonSecond.innerText = "Показать еще"
+    }
+});
+buttonThird.addEventListener("click", () => {
+  if (third.style.display === "none" || third.style.display === "") {
+      third.style.display = "block";
+      buttonThird.innerText = "Скрыть"
+  } else {
+      third.style.display = "none"; // Скрываем элемент
+      buttonThird.innerText = "Показать еще"
+  }
+});
